3c1fcd95ecc45e0d1666c517ef5cc3d3c9b10277
Ricky Clegg <ricky.clegg@transferwise.com>
2023-05-11 11:42:55 +0100

    fix: add linting to testing

--- black-box-tests/acceptance.ts
+++ black-box-tests/acceptance.ts
@@ -1,5 +1,6 @@
 console.log('Running Acceptance Tests')
 
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 function fail() {
   console.log('\x1b[31m%s\x1b[0m', 'Acceptance Tests Failed')
   process.exit(1)
