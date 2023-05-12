5e22eef9e2bb798913746e3e47b361fc83c87827
Ricky Clegg <ricky.clegg@transferwise.com>
2023-05-12 14:38:18 +0100

    refactor: add cdc to replace mock data

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,16 @@
 import express, { Express, Request, Response } from 'express'
 import { writeFileSync } from 'fs'
 import YAML from 'yaml'
+import fetch from 'node-fetch'
 
 const app: Express = express()
 
-app.get('/api/webhook', (req: Request, res: Response) => {
-  writeFileSync('pokemon.yml', YAML.stringify({ names: ['bulbasaur'] }))
+app.get('/api/webhook', async (req: Request, res: Response) => {
+  const data = await fetch('http://localhost:4000/pokemon')
+
+  const pokemonJson = await data.json()
+
+  writeFileSync('pokemon.yml', YAML.stringify(pokemonJson))
 
   res.sendStatus(200)
 })
