5da8d56b7b71a96cc1700e639a6461abd361e2cb
Ricky Clegg <ricky.clegg@transferwise.com>
2023-07-14 11:39:21 +0100

    wip: add a skipping failing test for next story

--- black-box-tests/acceptance.spec.ts
+++ black-box-tests/acceptance.spec.ts
@@ -14,6 +14,19 @@ describe('Acceptance tests', () => {
     expect(data.names[0]).toEqual('bulbasaur')
   })
 
+  xit('should create a bulbasaur.yml when the webhook is called', async () => {
+    const namesFile = 'pokemon.yml'
+    const pokemonFile = 'bulbasaur.yml'
+    await triggerWebhook()
+
+    const data = await readFile(pokemonFile)
+
+    removeFile(namesFile)
+    removeFile(pokemonFile)
+
+    expect(data.id).toEqual(1)
+  })
+
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   async function triggerWebhook() {
     await fetch('http://localhost:3000/api/webhook')
